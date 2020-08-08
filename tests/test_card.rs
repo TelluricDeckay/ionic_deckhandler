8cd43dc69872b388419d15069ac491ec3cf73033
Jammyjamjamman <jdsherratt3@gmail.com>
2020-08-08 19:40:30 +0100

    Change project to lib, add tests

---
+++ tests/test_card.rs
@@ -0,0 +1,15 @@
+use ionic_deckhandler::DeckShuffler;
+
+#[test]
+fn test_create_deck() {
+    let mut deck = ionic_deckhandler::Card::get_deck();
+    {
+        let first_card = &deck[0];
+        assert_eq!(format!("{}", first_card.get_suit()), String::from("♣"));
+    }
+    deck.shuffle_deck();
+    
+    for card in deck.iter() {
+        println!("{}", card);
+    }
+}
